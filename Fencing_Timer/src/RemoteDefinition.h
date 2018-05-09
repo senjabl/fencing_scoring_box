ca74338e23cfe623a249fdcefbe2f753d8bfd291
Senja <senja.jak@gmail.com>
2018-05-09 13:59:15 +0200

    Add bluetooth remote codes

--- Fencing_Timer/src/RemoteDefinition.h
+++ Fencing_Timer/src/RemoteDefinition.h
@@ -1,4 +1,5 @@
 // Definicija tastera na daljinskom s e-bay-a
+/*
 #ifndef IR_CARMP3
 
 #define IR_START_STOP 0xFFA25D // Code for "START/STOP time" button [CH-]
@@ -20,7 +21,34 @@
 #define IR_RUN_MODE 0xFF42BD //Code for "Run MODE" button [7]
 #define IR_MINUTES_PLUS 0xFF38C7 //Code for "MINUTES +" button --> Duration of match in settings [5]
 #define IR_MINUTES_MINUS 0xFF4AB5 //Code for "MINUTES -" button --> Duration of match in settings [8]
-#define IR_POINTS_PLUS 0xFF5AA5 //Code for "POINTS +" button --> Maximum number of points in match [6]
-#define IR_POINTS_MINUS 0xFF52AD //Code for "POINTS -" button --> Maximum number of points in match [9]
+#define IR_SECONDS_PLUS 0xFF5AA5 //Code for "SECONDS +" button --> Adjust seconds - increase [6]
+#define IR_SECONDS_MINUS 0xFF52AD //Code for "SECONDS -" button --> Adjust ceconds - decrease [9]
+
+#endif
+*/
+
+#ifndef BT_REMOTE
+
+#define IR_START_STOP 10 // Code for "START/STOP time" button [CH-]
+#define IR_PAUSE_1M 11 // Code for "PAUSE 1 minute" button [CH]
+#define IR_REARM 12 //Code for "Rearm all" button [CH+]
+#define IR_L_TOUCH_PLUS 13 //Code for "LEFT opponent touch +" button [|<<]
+#define IR_L_TOUCH_MINUS 14 //Code for "LEFT opponent touch -" button[-]
+#define IR_PERIOD_PLUS 15 //Code for "Mach PERIOD +" button [>>|]
+#define IR_PERIOD_MINUS 16 //Code for "Mach PERIOD -" button [+]
+#define IR_R_TOUCH_PLUS 17 //Code for "RIGHT opponent touch +" button [>||]
+#define IR_R_TOUCH_MINUS 18 //Code for "RIGHT opponent touch -" button [EQ]
+#define IR_L_YEL_CARD 19 //Code for "LEFT opponent YELLOW card" button [0]
+#define IR_L_RED_CARD 20 //Code for "LEFT opponent RED card" button [1]
+#define IR_R_YEL_CARD 21 //Code for "RIGHT opponent YELLOW card" button [200+]
+#define IR_R_RED_CARD 22 //Code for "RIGHT opponent RED card" button [3]
+#define IR_RESET_CARDS 23 //Code for "Reset all cards" button [2]
+#define IR_ADVANTAGE 24 //Code for "ADVANTAGE"button [100+]
+#define IR_SETTINGS 25 // Code for "Enter SETTINGS" button [4]
+#define IR_RUN_MODE 26 //Code for "Run MODE" button [7]
+#define IR_MINUTES_PLUS 27 //Code for "MINUTES +" button --> Duration of match in settings [5]
+#define IR_MINUTES_MINUS 28 //Code for "MINUTES -" button --> Duration of match in settings [8]
+#define IR_SECONDS_PLUS 29 //Code for "SECONDS +" button --> Adjust seconds - increase [6]
+#define IR_SECONDS_MINUS 30 //Code for "SECONDS -" button --> Adjust ceconds - decrease [9]
 
 #endif
